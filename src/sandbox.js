869319bacde1fd5883201e5e9984e43628eb9fc7
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-01-19 23:14:26 +0300

    refactor: add sandbox.js

---
+++ src/sandbox.js
@@ -0,0 +1,13 @@
+
+  
+  const units_arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const dozen_arr = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const dozens_arr = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  
+  let int_number = 123;
+
+  function toReadable (number) {
+    
+  }
+  
+  
