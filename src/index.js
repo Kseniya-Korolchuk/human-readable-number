4500f061e8d0d72de808d0439c5d64189a6b7faf
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-01-27 16:48:28 +0300

    fix: delete spaces in dozens

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
 module.exports = function toReadable (number) {
     const units_arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     const dozen_arr = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    const dozens_arr = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const dozens_arr = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     
-    let unit = '';
+    let units = '';
     let dozen = '';
     let dozens = '';
     let hundred = '';
-  
-    function toReadable (number) {
+
       if (number > 99) {
         if (number % 100 == 0) {
-         hundred = units_arr[number/100] + ' hundred';
+         hundred = units_arr[number/100] + ' ' + 'hundred';
          return hundred;
         }
        else if (number % 100 > 0) {
-         hundred = units_arr[(number - number % 100 )/ 100] + ' hundred ';
+         hundred = units_arr[(number - number % 100 )/ 100] + ' ' + 'hundred' + ' ';
          number = number % 100;
         }
       }
   
       if (number < 10) {
-        unit = units_arr[number];
+        units = units_arr[number];
+        return hundred + units;
       }
       else if (number >= 10 && number < 20) {
         dozen = dozen_arr[number % 10];
+        return hundred + dozen;
       }
       else if (number >= 20 && number < 100) {
-        unit = (number % 10 != 0) ? units_arr[number % 10] : ''; 
-        dozens = dozens_arr[(number - number % 10) / 10] + ' ';
+        units = (number % 10 != 0) ? ' ' + units_arr[number % 10] : ''; 
+        dozens = dozens_arr[(number - number % 10) / 10];
+        return hundred + dozens + units;
       }
-          return hundred + dozens + dozen + unit;
-    }
-}  
+}
